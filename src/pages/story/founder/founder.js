8e63b1075be86fd26cb792f48c094fc25cdae4dc
Andrew Southwick <asouthwick01@gmail.com>
2017-11-17 17:51:02 -0700

    styles for our story

--- src/pages/story/founder/founder.js
+++ src/pages/story/founder/founder.js
@@ -16,7 +16,7 @@ class Founder extends Component{
             <Col lg={{size:4, offset:1}} className="founder">
               <img src={require('./../../../images/Screen Shot 2017-11-16 at 3.47.00 PM.png')} alt=""className="founder-img"/>
             </Col>
-            <Col lg={{size:7}}>
+            <Col lg={{size:7}} sm="12">
                 <p>
                   Since 2008 we've taught everyday people the skills necessary to become great traders. Whether they want to make a part time or full time income that is up to them.  Our proprietary indicators give our students the edge necessary to successfully trade in today's markets. We teach our students one on one twice a week for 9 to 12 weeks which allows us to develop their skills and create a personalized trading plan centered around our proprietary indicators. Upon completion of their training we will fund our students day trading accounts to jump start them into live trading. We do quarterly reviews and if profitability is maintained we continue to add funds to their day trading account (up to $100,000) with our students keeping 60 to 80% of the profits. We are truly committed to our students success.
                 </p>
