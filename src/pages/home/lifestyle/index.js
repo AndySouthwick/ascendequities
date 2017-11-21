dfacabbd3c8c1f3db9ee02e9c5a7d4e82ff9c19f
Andrew Southwick <asouthwick01@gmail.com>
2017-11-21 14:50:37 -0700

    small text edit

--- src/pages/home/lifestyle/index.js
+++ src/pages/home/lifestyle/index.js
@@ -49,7 +49,7 @@ class Lifestyle extends Component{
                 <input  id="inf_field_LastName" name="inf_field_LastName" placeholder="Last Name *" type="text" />
                 <input  id="inf_field_Email" name="inf_field_Email" placeholder="Email *" type="text" />
                 <input  id="inf_field_Phone1" name="inf_field_Phone1" placeholder="Phone *" type="text" />
-                <i>If you were referred please leve the name of your referrer</i>
+                <i>If you were referred please leave the name of your referrer</i>
                 <input className="infusion-field-input-container" id="inf_custom_ReferralName" name="inf_custom_ReferralName" placeholder="Referral Name" type="text" />
                 <input name="inf_field_LeadSourceId" type="hidden" value="74" />
                 <button className="btn pill" id="recaptcha_3c7bdb3102ef40acb0e100ee2530995a" type="submit">Apply Now</button>
