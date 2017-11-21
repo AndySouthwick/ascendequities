5453ce8e8a67a3134e44458492d677bfee9ed306
Andrew Southwick <asouthwick01@gmail.com>
2017-11-21 13:34:09 -0700

    added loading spinner

--- src/pages/students/index.js
+++ src/pages/students/index.js
@@ -4,13 +4,22 @@ import Footer from './../../components/footer/footer'
 import {graphql}  from 'react-apollo'
 import gql from 'graphql-tag'
 import './students.css'
+import FontAwesome from 'react-fontawesome'
 class Students extends Component{
   render(){
 
     if(!this.props.data.allStudentsblurbses){
       return(
-        <div>
-          ...Loading
+        <div className="d-flex align-items-center justify-content-center">
+
+          <FontAwesome
+            className='center-spinner'
+            name='spinner'
+            size='4x'
+            spin
+            style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+          />
+        <h1 className="center-h1-loading">Loading</h1>
         </div>
       )
     }else
