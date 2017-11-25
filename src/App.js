6ec7a810e8dcfbb1f9e37b1f9366599c3f448220
Andrew Southwick <asouthwick01@gmail.com>
2017-11-25 14:42:15 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
-import Home from './pages/home/home'
-import Contact from './pages/contact/contact'
-import Students from './pages/students'
-import OurStory from './pages/story'
 import Apply from './pages/apply/apply'
 
 class App extends Component {
@@ -13,11 +9,7 @@ class App extends Component {
   render() {
     return (
         <Switch>
-          <Route exact path="/:id" component={Apply}/>
-          <Route exact path="/contact" component={Contact}/>
-          <Route exact path="/Students" component={Students}/>
-          <Route exact path="/story" component={OurStory}/>
-          <Route exact path="/apply-now/:id" component={Apply}/>
+          <Route exact path="/#/:id" component={Apply}/>
         </Switch>
 
     );
